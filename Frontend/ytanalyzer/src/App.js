feff8649a847c2205ca8ecab908c0daad501927b
Ansh <anshsrivasrava2004@gmail.com>
2025-02-07 01:16:40 +0530

    new frontend folder

--- Frontend/ytanalyzer/src/App.js
+++ Frontend/ytanalyzer/src/App.js
@@ -1,13 +1,22 @@
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          YtAnalyzer
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Analyze all your YT comments in one place</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
